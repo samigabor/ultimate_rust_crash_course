babf79530b1a30aeb175ac0a3c122a68405ea0ae
samigabor <samoila.gabor@yahoo.com>
2023-11-29 11:18:22 +0200

    struct & trait exercise

--- exercise/f_structs_traits/src/main.rs
+++ exercise/f_structs_traits/src/main.rs
@@ -1,48 +1,38 @@
 // 1. Define a trait named `Bite`
-//
 // Define a single required method, `fn bite(self: &mut Self)`.  We will call this method when we
-// want to bite something.  Once this trait is defined, you should be able to run the program with
-// `cargo run` without any errors.
-//
-//  trait Bite...
+// want to bite something.
+trait Bite {
+    fn bite(self: &mut Self);
+}
 
 
-// 2. Now create a struct named Grapes with a field that tracks how many grapes are left.  If you
-// need a hint, look at how it was done for Carrot at the bottom of this file (you should probably
-// use a different field, though).
-//
-// #[derive(Debug)] // include this line right before your struct definition
-// struct Grapes...
-
-
-// 3. Implement Bite for Grapes.  When you bite a Grapes, subtract 1 from how many grapes are left.
-// If you need a hint, look at how it was done for Carrot at the bottom of this file.
-//
-// impl Bite for...
-
+// 2. Now create a struct named Grapes with a field that tracks how many grapes are left.
+#[derive(Debug)] // include this line right before your struct definition
+struct Grapes {
+    amount_left: i32,
+}
 
 fn main() {
-    // Once you finish #1 above, this part should work.
     let mut carrot = Carrot { percent_left: 100.0 };
     carrot.bite();
     println!("I take a bite: {:?}", carrot);
 
-    // 4. Uncomment and adjust the code below to match how you defined your
-    // Grapes struct.
-    //
-    //let mut grapes = Grapes { amount_left: 100 };
-    //grapes.bite();
-    //println!("Eat a grape: {:?}", grapes);
+    let mut grapes = Grapes { amount_left: 100 };
+    grapes.bite();
+    println!("Eat a grape: {:?}", grapes);
 
-    // Challenge: Uncomment the code below. Create a generic `bunny_nibbles`
-    // function that:
+    // Generic `bunny_nibbles` function that:
     // - takes a mutable reference to any type that implements Bite
     // - calls `.bite()` several times
-    // Hint: Define the generic type between the function name and open paren:
-    //       fn function_name<T: Bite>(...)
-    //
-    //bunny_nibbles(&mut carrot);
-    //println!("Bunny nibbles for awhile: {:?}", carrot);
+    fn bunny_nibbles<T: Bite>(food: &mut T) {
+        println!("Bunny is nibbling...");
+        food.bite();
+        println!("Bunny is nibbling some more...");
+        food.bite();
+    }
+    
+    bunny_nibbles(&mut carrot);
+    println!("Bunny nibbles for awhile: {:?}", carrot);
 }
 
 #[derive(Debug)] // This enables using the debugging format string "{:?}"
@@ -54,5 +44,13 @@ impl Bite for Carrot {
     fn bite(self: &mut Self) {
         // Eat 20% of the remaining carrot. It may take awhile to eat it all...
         self.percent_left *= 0.8;
+    }
+}
+
+// 3. Implement Bite for Grapes. When you bite a Grapes, subtract 1 from how many grapes are left.
+impl Bite for Grapes {
+    fn bite(&mut self) {
+        // Eat 1 grape of the remaining grapes. It may take awhile to eat it all...
+        self.amount_left -= 1;
     }
 }
